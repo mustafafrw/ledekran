c0d8aa373c5b6c35c4ae309b9474bffc88600054
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2020-03-05 21:34:30 +0300

    easd

--- application/controllers/kategori.php
+++ application/controllers/kategori.php
@@ -7,18 +7,38 @@ class Kategori extends CI_Controller {
   public function __construct() {
       
       parent::__construct();
-      $this->viewFolder = "includes";
+      $this->viewFolder = "kategori";
       $this->load->model("main_model");
   }
-
+    
     function _remap($param) {
         $this->index($param);
     }
 
     function index($param){
         echo $param;
+        $header = $this->main_model->get_all(
+            array("type" => "head"), "menu_id ASC","menu"
+        );
+        $footer = $this->main_model->get_all(
+            array("type" => "footer"), "menu_id ASC","menu"
+        );
+        $slider = $this->main_model->get_all(
+            array(), "id ASC","slider"
+        );
+        $items = array(
+            "header" => $header,
+            "footer" => $footer,
+            "slider" => $slider
+        );
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->items = $items;
+
+        $this->load->view("{$viewData->viewFolder}/index", $viewData);
     }
-    /*public function pr($id){
-        echo $id;
-    }*/
+
+    
+        
+    
 }
